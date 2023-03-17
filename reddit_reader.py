bca04a0852cecb82dfe5997204610c2b065e1eab
JonasSto <jonas.stoeckermann@gmx.de>
2023-03-17 10:19:03 +0100

    2. Versuch von Rausch akzeptiert zu werden

--- reddit_reader.py
+++ reddit_reader.py
@@ -1,38 +1,34 @@
-import praw 
+from thread import Thread
 
-class Thread:
-    def __init__(self, title, body, gain_or_loss):
-        self.title = title
-        self.body = body
-        self.gain_or_loss = gain_or_loss
+import yaml
+import praw 
 
 class RedditAPI:
     # Set up the Reddit API client
     def __init__(self):
-        self.reddit = praw.Reddit(client_id='8FLiiST3e2FoD-iKhoQKAw',
-                            client_secret='agWaeCM9Rdc1ziqQUbitJVeyDB0zZg',
+        # Load the config file 
+        with open('resource.yaml', 'r') as f:
+            data = yaml.safe_load(f)
+
+        # Retrieve reddit api access data
+        reddit_api_access_data = data['reddit_api_access_data']
+        reddit_client_id = reddit_api_access_data['client_id']
+        reddit_client_secret = reddit_api_access_data['client_secret']
+
+        # Create reddit client
+        self.reddit = praw.Reddit(client_id=reddit_client_id,
+                            client_secret=reddit_client_secret,
                             user_agent='read wallstreetbets threads')
 
     # Retrieve a thread from r/wallstreetbets with the "Gain/Loss","day","top" filters + index(top to bottom)
-    def get_post(self, index):
+    def get_posts_of_day(self, index):
         subreddit = self.reddit.subreddit('wallstreetbets')
         posts = list(subreddit.search('flair:"Gain" OR flair:"Loss"', time_filter='day', sort='top', limit=index+1))
         if len(posts) >= index+1:
-            top_post = posts[index]
-            if top_post.link_flair_text == "Gain":
-                return Thread(top_post.title, top_post.selftext, "gain")
-            elif top_post.link_flair_text == "Loss":
-                return Thread(top_post.title, top_post.selftext, "loss")
+            filtered_thread = posts[index]
+            if filtered_thread.link_flair_text == "Gain":
+                return Thread(filtered_thread.title, "gain")
+            elif filtered_thread.link_flair_text == "Loss":
+                return Thread(filtered_thread.title, "loss")
         else:
             return None
-    
-# Example usage
-redditClient = RedditAPI()
-thread = redditClient.get_post(0)
-print(thread.__dict__)
-thread = redditClient.get_post(1)
-print(thread.__dict__)
-thread = redditClient.get_post(2)
-print(thread.__dict__)
-thread = redditClient.get_post(3)
-print(thread.__dict__)
