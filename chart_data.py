562affed7547114df597a65381762cb479ba801a
Ozan <akcebe.ozn@gmail.com>
2023-05-11 09:59:23 +0200

    add not working stock

--- chart_data.py
+++ chart_data.py
@@ -23,6 +23,7 @@ class ChartData:
                     data = json.loads(price_string)
                     value += amount * float(data['high'])
                 except:
+                    value += amount * stock[2]
                     print("Exception occured for stock " + stock[0])
 
         self.db.insert_into_chart(date, bank+value)
