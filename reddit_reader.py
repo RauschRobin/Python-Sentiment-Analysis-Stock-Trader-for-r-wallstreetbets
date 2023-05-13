f3c0bc2e4c966df6dbc01603d68d14bfb632fa81
JonasSto <64435492+JonasSto@users.noreply.github.com>
2023-05-13 17:48:49 +0200

    Update reddit_reader.py

--- reddit_reader.py
+++ reddit_reader.py
@@ -6,47 +6,45 @@ import os
 class RedditAPI:
     # Set up the Reddit API client
     def __init__(self):
-        '''
-        Initializes the RedditAPI. This is the constructor. It loads the reddit api key and prepares praw to be used.
+        """
+        Initializes the Reddit API client.
 
-        Parameters:
-        None
+        This method reads the configuration file and sets up the Reddit API client using the provided access data.
 
-        Returns:
-        None
+        param: None
 
-        Tests:
+        :return: None
 
-
-        
-        '''
-        # Load the config file
+        :test:
+        * test 1: Instantiate the RedditAPIClient class and verify that the reddit attribute is initialized and is an instance of praw.Reddit. 
+        * test 2: Simulate a missing or incorrect configuration file by either creating an empty configuration file or modifying an existing one. 
+                  Instantiate the RedditAPIClient class and verify that the reddit attribute is not initialized. 
+        """
         dir_path = os.path.dirname(os.path.realpath(__file__))
         yaml_path = os.path.join(dir_path, 'resource.yaml')
         with open(yaml_path, 'r') as f:
             data = yaml.safe_load(f)
 
-        # Retrieve reddit api access data
         reddit_api_access_data = data['reddit_api_access_data']
         reddit_client_id = reddit_api_access_data['client_id']
         reddit_client_secret = reddit_api_access_data['client_secret']
 
-        # Create reddit client
         self.reddit = praw.Reddit(client_id=reddit_client_id,
                             client_secret=reddit_client_secret,
                             user_agent='read wallstreetbets threads')
 
-    # Retrieve a thread from r/wallstreetbets with the "day","top" filters + index(top to bottom)
     def get_posts_of_day(self, index):
-        '''
+        """
         This functions returns the n'th post of the top posts of that day. You can set which top post you want by setting the parameter index.
 
-        Parameters:
-        index : int
+        :param index: on what index to get the post
 
-        Returns:
-        a new post: Thread object
-        '''
+        :return: The title of the post in form of a Thread object
+
+        :test:
+        * test 1: Retrieve post on index 10 and check if return value is not none
+        * test 2: Attempt to retrieve a post beyond the available posts for the day. Return value should be none
+        """
         subreddit = self.reddit.subreddit('wallstreetbets')
         posts = list(subreddit.top(time_filter="day"))
         if len(posts) >= index+1:
