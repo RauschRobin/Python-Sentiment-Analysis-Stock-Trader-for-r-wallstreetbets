dd7f6e9f4899e41dab47c412618364d131e4bf12
JonasSto <jonas.stoeckermann@gmx.de>
2023-05-13 21:18:14 +0200

    praw --> asyncpraw

--- reddit_reader.py
+++ reddit_reader.py
@@ -1,10 +1,9 @@
 from thread import Thread
 import yaml
-import praw
+import asyncpraw
 import os
 
 class RedditAPI:
-    # Set up the Reddit API client
     def __init__(self):
         """
         Initializes the Reddit API client.
@@ -29,11 +28,11 @@ class RedditAPI:
         reddit_client_id = reddit_api_access_data['client_id']
         reddit_client_secret = reddit_api_access_data['client_secret']
 
-        self.reddit = praw.Reddit(client_id=reddit_client_id,
+        self.reddit = asyncpraw.Reddit(client_id=reddit_client_id,
                             client_secret=reddit_client_secret,
                             user_agent='read wallstreetbets threads')
-
-    def get_posts_of_day(self, index):
+        
+    async def get_posts_of_day(self, index):
         """
         This functions returns the n'th post of the top posts of that day. You can set which top post you want by setting the parameter index.
 
@@ -45,8 +44,10 @@ class RedditAPI:
         * test 1: Retrieve post on index 10 and check if return value is not none
         * test 2: Attempt to retrieve a post beyond the available posts for the day. Return value should be none
         """
-        subreddit = self.reddit.subreddit('wallstreetbets')
-        posts = list(subreddit.top(time_filter="day"))
+        subreddit = await self.reddit.subreddit('wallstreetbets')
+        posts = []
+        async for submission in subreddit.top(time_filter="day"):
+            posts.append(submission)
         if len(posts) >= index+1:
             filtered_thread = posts[index]
             return Thread(filtered_thread.title)
